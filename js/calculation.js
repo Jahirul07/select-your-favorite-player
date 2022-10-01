58ec936b6ed914b573543a9ca83d6d26e3920da7
Jahirul Islam <jahirul@elyranur.com>
2022-10-02 01:09:29 +0600

    calculation js done

---
+++ js/calculation.js
@@ -0,0 +1,17 @@
+document.getElementById("calculate-btn").addEventListener("click", function () {
+    const playerInputField = document.getElementById("player-budget").value;
+    const playerCost = playerInputField * 5;
+    const playerTotalCost = document.getElementById("player-expenses");
+    playerTotalCost.innerText =playerCost;
+
+
+    document.getElementById("total-expenses").addEventListener("click", function () {
+        const managerFeeString = document.getElementById("manager-budget").value;
+        const managerFee = parseInt(managerFeeString);
+        const coachFeeString = document.getElementById("coach-budget").value;
+        const coachFee = parseInt(coachFeeString);
+        const totalCost = playerCost + managerFee + coachFee;
+        document.getElementById("total-cost").innerHTML = totalCost;
+    })
+
+})
